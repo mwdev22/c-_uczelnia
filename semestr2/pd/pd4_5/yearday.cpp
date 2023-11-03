0e0607d972cb4c1f6eb12ae26d737daa427aa539
macwojDev <maciekwojdyna1@gmail.com>
2023-11-03 12:10:33 +0100

    lekka rekonstrukcja yearday

--- semestr2/pd/pd4_5/yearday.cpp
+++ semestr2/pd/pd4_5/yearday.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 #include <ctime>
 
-int main() {
-    int y, m, d;
-    std::cin >> y >> m >> d;
-
+int calculate_yearday(int y, int m, int d){
     // inicjalizacja struktury
     tm yearday;
     yearday.tm_year = y - 1900; // rok musi być przesunięty o 1900, ponieważ od 1900 jest indeksowany
     yearday.tm_mon = m - 1; // miesiące są indeksowane od 0
     yearday.tm_mday = d;
-
     // pola dotyczące godziny ustawiamy na 0
     yearday.tm_hour = 0;
     yearday.tm_min = 0;
@@ -18,5 +14,11 @@ int main() {
 
     std::mktime(&yearday);
 
-    std::cout << yearday.tm_yday + 1 << std::endl; // +1, ponieważ tm_yday indeksowane jest od 0
+    return yearday.tm_yday + 1;
+}
+
+int main() {
+    int y, m, d;
+    std::cin >> y >> m >> d;
+    std::cout << calculate_yearday(y, m, d) << std::endl;
 }
